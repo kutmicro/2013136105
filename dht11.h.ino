739bf0ad96b858acab5958125954ae751d38ee08
2013136082 <kutmicro@gmail.com>
2016-11-01 00:21:07 +0900

    온습도센서 코드

---
+++ dht11.h.ino
@@ -0,0 +1,14 @@
+#include <DHT11.h>
+#define pin A0
+
+DHT11 t(pin);
+void setup() {
+  Serial.begin(9600);
+}
+void loop() {
+  int i;
+  float temp, humi;
+  if((i = t.read(humi, temp)) == 0) {
+    Serial.println(humi);
+  }
+}
