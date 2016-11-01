7a89928371f2a0fda37a0a5ca47c6a0c72a19480
2013136082_R-pet'sSafeHabitat <kutmicro@gmail.com>
2016-11-01 16:58:48 +0900

    Add files via upload

---
+++ Stepper_1.ino
@@ -0,0 +1,31 @@
+#include <Stepper.h>
+#include <SoftwareSerial.h>
+
+int TxPin = 2;
+int RxPin = 3;
+int Button = 7;
+int stepsmotor = 1024;
+SoftwareSerial BTSerial(TxPin, RxPin);
+
+Stepper myStepper(stepsmotor, 8, 10, 9, 11);
+void setup() {
+  BTSerial.begin(9600);
+  myStepper.setSpeed(22);
+  pinMode(Button, INPUT);
+}
+
+void loop() {
+  if(digitalRead(Button) == LOW) {
+    while(digitalRead(Button) == LOW)
+    myStepper.step(stepsmotor);
+  }
+  if(BTSerial.available()) {
+    char cmd = (char)BTSerial.read();
+    if(cmd == '1')
+      myStepper.step(stepsmotor);
+
+    else if(cmd == '2') 
+      myStepper.step(-stepsmotor);
+  }
+}
+
