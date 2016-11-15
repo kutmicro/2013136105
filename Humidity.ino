c8c5f076c0e6c47f19ae671d0cce0480802ee2be
2013136082_R-pet'sSafeHabitat <kutmicro@gmail.com>
2016-11-15 16:22:59 +0900

    Update Humidity.ino

--- Humidity.ino
+++ Humidity.ino
@@ -1,17 +1,17 @@
 #include <dht11.h>
-#define DHT11PIN 12 //12번핀을 통해 온도와 습도정보를 전달합니다.
+#define DHT11PIN 12 // 12번핀을 통해 온도와 습도정보를 전달합니다.
 dht11 DHT11;
 void setup() {
-  Serial.begin(9600);//시리얼통신 속도를 9600으로 맞춥니다.
+  Serial.begin(9600);// 시리얼통신 속도를 9600으로 맞춥니다.
   Serial.println("start");
 
 }
 
 void loop() {
  Serial.println();
- int chk=DHT11.read(DHT11PIN);//7번핀을 통해 정보를 읽습니다.
+ int chk=DHT11.read(DHT11PIN);// 12번핀을 통해 정보를 읽습니다.
  Serial.println("Read");
- switch(chk){//상태를 체크합니다. ok가나와야 정상입니다.
+ switch(chk){ //상태를 체크합니다. ok가나와야 정상입니다.
   case 0:Serial.println("ok");break; 
   case -1:Serial.println("checksum error"); break;
   case -2:Serial.println("time out"); break; 
