88262998c8d3dcbc063aa9d82cc81038165d080d
2013136082_R-pet'sSafeHabitat <kutmicro@gmail.com>
2016-12-06 16:39:33 +0900

    Add files via upload

---
+++ Bluetooth.ino
@@ -0,0 +1,17 @@
+char BT_data;
+char MEGA_data;
+void setup() {
+  Serial.begin(9600);
+  Serial1.begin(9600);
+}
+void loop() {
+  if(Serial1.available()) {
+    BT_data=Serial1.read();
+    Serial.println(BT_data);
+    }
+  else if (Serial.available()) {
+    MEGA_data=Serial.read();
+    Serial1.println(MEGA_data);
+    }
+}
+
---
+++ CDS.ino
@@ -0,0 +1,20 @@
+#define POT_PIN A8
+#define LED_PIN 13 
+
+void setup()
+{
+  pinMode(LED_PIN, OUTPUT); 
+}
+void loop()
+{
+  int val = 0;
+  val = analogRead(POT_PIN); 
+
+  //조도센서 값이 100미만이면 LED를 켠다
+  if(val < 100) {
+    digitalWrite(LED_PIN, HIGH);
+  } else {
+    digitalWrite(LED_PIN, LOW);
+  }
+  delay(500);
+}
